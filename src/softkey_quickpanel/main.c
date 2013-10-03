42806ea24fbdd61f0f7399bf142b3a0de0879b1b
Sebastian Krzyszkowiak <dos@dosowisko.net>
2013-10-03 17:09:39 +0200

    shr_elm_softkey: proper fix for #1490
    Signed-off-by: Sebastian Krzyszkowiak <dos@dosowisko.net>

--- src/softkey_quickpanel/main.c
+++ src/softkey_quickpanel/main.c
@@ -245,8 +245,8 @@ on_win_select(void *data, Evas_Object *obj, void *event)
 
 	ecore_x_netwm_client_active_request(root, window->xwin, 0, tasklist);
 
-	//commenting the next line fixes the SHR bug #1490
-	//evas_object_del(tasklist_win);
+	evas_object_del(tasklist_win);
+	tasklist_win = NULL;
 	winlist_free_if_not_null();
 }
 
